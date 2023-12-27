710ce794193aca681893da307118a8bf686097d3
keshavjain2006 <keshukeshavjain@gmail.com>
2023-12-27 22:47:42 +0530

    first commit

---
+++ precache-manifest.1448bc7e9afd55e25ab5ac3b081ca59d.js
@@ -0,0 +1,142 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f2bf68d96f0b38e32e26ff8c0de9fdec",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ab6097d78006c06bb3aa",
+    "url": "/static/css/3.df879eac.chunk.css"
+  },
+  {
+    "revision": "fa7c2203bb3b0366edb6",
+    "url": "/static/css/4.d7ceb645.chunk.css"
+  },
+  {
+    "revision": "8c3347689f44ef9b691e",
+    "url": "/static/css/main.d5b3eed5.chunk.css"
+  },
+  {
+    "revision": "d53e26e5b29f91b6592e",
+    "url": "/static/js/2.eca4b8bd.chunk.js"
+  },
+  {
+    "revision": "d4d701abc30fcef5f714584a64df6e82",
+    "url": "/static/js/2.eca4b8bd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ab6097d78006c06bb3aa",
+    "url": "/static/js/3.4156fd8e.chunk.js"
+  },
+  {
+    "revision": "fa7c2203bb3b0366edb6",
+    "url": "/static/js/4.968d4f55.chunk.js"
+  },
+  {
+    "revision": "8c3347689f44ef9b691e",
+    "url": "/static/js/main.799b0c15.chunk.js"
+  },
+  {
+    "revision": "772ed464baebd344fc83",
+    "url": "/static/js/runtime-main.f1021d4a.js"
+  },
+  {
+    "revision": "21f233e19402cc4a66866a7f31191f0d",
+    "url": "/static/media/Agustina.21f233e1.woff"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "ed66dd734f3087f8fcb2dd2467b44538",
+    "url": "/static/media/TIMO_Logo.ed66dd73.png"
+  },
+  {
+    "revision": "afec9505754a96e39f23a90641eca9f3",
+    "url": "/static/media/airbnb.afec9505.webp"
+  },
+  {
+    "revision": "ffaf6d955b52940d2d85b5ebc3c127a5",
+    "url": "/static/media/beamlineforschools.ffaf6d95.png"
+  },
+  {
+    "revision": "dfdb78936d0e1c3487a787905b0674ac",
+    "url": "/static/media/caring_hands.dfdb7893.jpeg"
+  },
+  {
+    "revision": "51cf7192327488da3d2a171280680c16",
+    "url": "/static/media/contactMailDark.51cf7192.svg"
+  },
+  {
+    "revision": "59389695208d7454c6607bed51ed4316",
+    "url": "/static/media/developerActivity.59389695.svg"
+  },
+  {
+    "revision": "1be13e7e3768d947f8a6e47fa0df4c48",
+    "url": "/static/media/dmkd.1be13e7e.jpeg"
+  },
+  {
+    "revision": "c96937ca9f60174f29fb0308d97b2bdb",
+    "url": "/static/media/goldman.c96937ca.png"
+  },
+  {
+    "revision": "82fcb19d1876cd376ea11eebbc1629d6",
+    "url": "/static/media/innovatex.82fcb19d.png"
+  },
+  {
+    "revision": "567f6429a5fdbbd35eeec13fe3b43f87",
+    "url": "/static/media/iosr_logo.567f6429.jpeg"
+  },
+  {
+    "revision": "8f3edda243b04af872c68f0961360cce",
+    "url": "/static/media/jpis.8f3edda2.jpeg"
+  },
+  {
+    "revision": "cee29891fd302271a3ea8bdfe49b163d",
+    "url": "/static/media/knowverse.cee29891.png"
+  },
+  {
+    "revision": "1cdd9f103d74e8c62fd8801c555e7fa0",
+    "url": "/static/media/lumiere_education_logo.1cdd9f10.jpeg"
+  },
+  {
+    "revision": "52be31441c8cf8a8a16bd7a228d428bf",
+    "url": "/static/media/manOnTable.52be3144.svg"
+  },
+  {
+    "revision": "d33da110a55f07cedc92a64abae1f9dc",
+    "url": "/static/media/pimso.d33da110.jpeg"
+  },
+  {
+    "revision": "6606c48fbf49fc629449aa11170b8c1c",
+    "url": "/static/media/python.6606c48f.png"
+  },
+  {
+    "revision": "3440939881da8339d1597e8d77cdf850",
+    "url": "/static/media/skill.34409398.svg"
+  },
+  {
+    "revision": "7537d434d4c7ad82663ec008d6681bc5",
+    "url": "/static/media/spacehack.7537d434.png"
+  },
+  {
+    "revision": "13780cdd0144cacdef25486e2c2503f8",
+    "url": "/static/media/talksCardBack.13780cdd.svg"
+  },
+  {
+    "revision": "6dda3e3893618f9696c15678c89650fd",
+    "url": "/static/media/tjpho.6dda3e38.png"
+  },
+  {
+    "revision": "36877e9e5494d2d0791ee751e3d7e67c",
+    "url": "/static/media/trs.36877e9e.png"
+  },
+  {
+    "revision": "c77462260ba81a611fe9fc775cddc15b",
+    "url": "/static/media/tweaking.c7746226.jpeg"
+  },
+  {
+    "revision": "7689df57c85a24463d255fb0141350e7",
+    "url": "/static/media/web-development.7689df57.png"
+  }
+]);
